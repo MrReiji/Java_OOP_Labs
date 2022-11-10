932662f4614a411f8d7566adc456a3d0a172713f
Gabriel Rosół <64375466+MrReiji@users.noreply.github.com>
2022-11-10 10:50:19 +0100

    Update FileCopy

--- Lab4/src/FileCopy.java
+++ Lab4/src/FileCopy.java
@@ -1,48 +1,52 @@
 import java.io.*;
-import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
 import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
-public class FileCopy {
-
+public class FileCopy{
     public static void main(String[] args) throws IOException {
-
-        if (args.length == 0) {
-            System.out.println("Brak argumentów programu.");
-            return;
-        }
-        File in = new File(args[0]);
-        if (in.isDirectory()){
-            System.out.println(args[0]+ " jest katalogiem");
-            return;
+        if (args.length < 2) {
+            System.out.println("Za mało argumentów wywołania programu !");
+            System.exit(1);
         }
 
-        File out = new File(args[1]);
+        File srcFile = new File(args[0]);
+        File dstFile = new File(args[1]);
 
-
-        FileChannel inChannel = new
-                FileInputStream(in).getChannel();
-        FileChannel outChannel = new
-                FileOutputStream(out).getChannel();
-
-
+        if (!srcFile.exists()) {
+            System.out.println("Plik " + srcFile.getName() + " nie istnieje !");
+            System.exit(1);
+        }
+        if (srcFile.isDirectory()) {
+            System.out.println(srcFile.getName() + " jest katalogiem !");
+            System.exit(1);
+        }
+        if (!srcFile.canRead()) {
+            System.out.println("Brak dostępu do pliku " + srcFile.getName() + " !");
+            System.exit(1);
+        }
+        if (dstFile.exists() && !dstFile.canWrite()) {
+            System.out.println("Nie można nadpisać pliku " + srcFile.getName() + " !");
+            System.exit(1);
+        }
+        if (dstFile.isFile() && !dstFile.canWrite()) {
+            System.out.println("Brak wymaganych uprawnień do zapisu pliku " + dstFile.getName() + " !");
+            System.exit(1);
+        }
+        if (dstFile.isDirectory()) {
+            if(!dstFile.canWrite()) {
+                System.out.println("Brak uprawnień do katalogu " + dstFile.getName() + " !");
+                System.exit(1);
+            }
+            dstFile = new File(Paths.get(dstFile.getName()) + "/" + srcFile.getName());
+        }
         try {
-            inChannel.transferTo(0, inChannel.size(),
-                    outChannel);
-        }
-        catch (FileNotFoundException e){
-            System.out.println("Z plikiem cos nie tak");
+            Files.copy(srcFile.toPath(), dstFile.toPath(), REPLACE_EXISTING);
         }
         catch (IOException e) {
-            System.out.println("Z plikiem cos nie tak");
+            System.out.println("Błąd związany z: " + e.getMessage());
+            System.exit(1);
         }
-
-        finally {
-            if (inChannel != null) inChannel.close();
-            if (outChannel != null) outChannel.close();
-        }
-
-
     }
-
 }
+
