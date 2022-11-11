3e98fe9617699d666a7cc5db7d8a1e5b08a20ab8
Gabriel Rosół <64375466+MrReiji@users.noreply.github.com>
2022-11-11 17:42:40 +0100

    Update WordCounter.java

--- Lab4/src/WordCounter.java
+++ Lab4/src/WordCounter.java
@@ -1,40 +1,87 @@
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 
 public class WordCounter {
+    private static String getArgs(String[] args) {
+        StringBuilder argString = new StringBuilder();
+        for (String arg : args) {
+            if (!arg.startsWith("-")) continue;
 
-    public static void main(String[] args) {
-
-        if (args.length==0) {
-            System.out.println("Brak argumentów programu.");
-            return;
-        }
-
-        try {
-            BufferedReader	reader = new BufferedReader(new FileReader(args[0]));
-            String line;
-            int lines = 0;
-            int characters = 0;
-            int words = 0;
-            while ((line = reader.readLine()) != null) {
-                characters += line.length();
-                words += (line.split("\\s+")).length;
-                lines++;
+            for (int i = 1; i < arg.length(); ++i) {
+                argString.append(arg.charAt(i));
             }
-            reader.close();
-            System.out.println("Wierszy: "+ lines);
-            System.out.println("Znaków: "+ characters);
-            System.out.println("Słów: "+ words);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        }
+        return argString.toString();
+    }
+
+    private static String getFilename(String[] args) {
+        for (String arg : args) {
+            if (!arg.startsWith("-")) {
+                return arg;
+            }
+        }
+        return null;
+    }
+
+    private static int[] getData(FileReader file) throws IOException {
+        BufferedReader readFile = new BufferedReader(file);
+        int lineCount = 0, charCount = 0, wordCount = 0;
+        String line;
+
+        while((line = readFile.readLine()) != null) {
+            lineCount++;
+            wordCount += line.split("\\s+").length;
+            charCount += line.length();
+        }
+        return new int[]{lineCount, charCount, wordCount};
+    }
+    public static void main(String[] args) {
+        FileReader file;
+        int[] result = null;
+        String filename = getFilename(args);
+        if (filename == null) {
+            System.out.println("Nie podano pliku !");
+            System.exit(1);
+        }
+        try{
+            file = new FileReader(filename); // reading file
+            result = getData(file);
+        }
+        catch(FileNotFoundException exception){
+            System.out.println("Plik nie istnieje !");
+            System.exit(1);
+        }
+        catch(ArrayIndexOutOfBoundsException exception){
+            System.out.println("Nie podano pliku, bądź podano kilka plików naraz !");
+            System.exit(1);
+        }
+        catch(IOException e){
+            System.out.println("Błąd IO !");
+            System.exit(1);
         }
 
+        String arguments = getArgs(args);
 
+        boolean isL = arguments.contains("l");
+        boolean isC = arguments.contains("c");
+        boolean isW = arguments.contains("w");
 
+        if(isL) {
+            System.out.println("wierszy: "  + result[0]);
+        }
+        if(isC) {
+            System.out.println("znaków: "  + result[1]);
+        }
+        if(isW) {
+            System.out.println("słów: "  + result[2]);
+        }
+        if(!isL && !isC && !isW) {
+            System.out.println("wierszy: "  + result[0]);
+            System.out.println("znaków: "  + result[1]);
+            System.out.println("słów: "  + result[2]);
+        }
 
     }
 
